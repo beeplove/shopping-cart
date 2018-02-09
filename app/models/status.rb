41b6e2f70388023dfec74c20ecdcfe18a3d96b91
Mohammad Khan <mohammad.khan@gmx.us>
2018-02-09 11:43:07 -0800

    added barebone model

---
+++ app/models/status.rb
@@ -0,0 +1,8 @@
+class Status < ApplicationRecord
+  #
+  # TODO
+  #   - Validation
+  #   - Index and Unique keys
+  #   - Association with Order
+  #
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209173433) do
+ActiveRecord::Schema.define(version: 20180209193635) do
 
   create_table "customers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "first_name", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20180209173433) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  create_table "statuses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
