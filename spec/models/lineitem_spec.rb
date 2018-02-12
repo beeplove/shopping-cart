5ef468d7b28b893a7d4f4529b045306965059684
Mohammad Khan <mohammad.khan@gmx.us>
2018-02-12 12:17:55 -0800

    added a failed test for lineitem.quantity to check decimal

--- spec/models/lineitem_spec.rb
+++ spec/models/lineitem_spec.rb
@@ -4,5 +4,21 @@ require 'rails_helper'
 # TODO
 #   - add coverage for price_in_cents
 RSpec.describe Lineitem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#quantity" do
+    let(:lineitem) { build(:lineitem) }
+
+    it "should validate quantity to be greater than 0" do
+      lineitem.quantity = 0
+      expect(lineitem.valid?).to eq(false)
+    end
+
+    it "should validate quantity by taking decimal_factor into consideration" do
+      lineitem.quantity = 1.3
+      expect(lineitem.valid?).to eq(false)
+    end
+  end
+
+  describe "#price_in_cents"
+  
 end
