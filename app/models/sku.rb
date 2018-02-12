477fd819cfb20e03991773398068e14a141c57e0
Mohammad Khan <mohammad.khan@gmx.us>
2018-02-12 14:07:22 -0800

    added test cases and updade model for sku

--- app/models/sku.rb
+++ app/models/sku.rb
@@ -2,14 +2,19 @@ class Sku < ApplicationRecord
   #
   # TODO
   #   - Add validations
-  #     - validation for price, negativity and not too high
-  #     - validation for decimal_factor, negativity and not too high
   #   - Add index and unique constraints
   #   - price accessor from price_in_cents
   #
 
   belongs_to :product
   belongs_to :unit
+
+  # TODO: for now keeping maximum unit price to 1,000,000 cents
+  validates :unit_price_in_cents, inclusion: { in: 1..1_000_000 }
+
+  # TODO: For now keeping decimal up to one-thousandth
+  validates :decimal_factor, inclusion: { in: 0..3 }
+
 
   has_many :lineitems
 end
--- spec/models/sku_spec.rb
+++ spec/models/sku_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Sku, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:sku) { build(:sku) }
+  describe "#unit_price_in_cents" do
+    it "should validate unit price to be greater than 0" do
+      sku.unit_price_in_cents = 0
+      expect(sku.valid?).to eq(false)
+    end
+
+  end
+
+  describe "#decimal_factor" do
+    it "should validate decimal factor anywhere between 0 and 3" do
+      sku.decimal_factor = 0
+      expect(sku.valid?).to eq(true)
+
+      sku.decimal_factor = 2
+      expect(sku.valid?).to eq(true)
+
+      sku.decimal_factor = 4
+      expect(sku.valid?).to eq(false)
+    end
+
+  end
 end
