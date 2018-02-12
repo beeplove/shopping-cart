617b84405aedd1e1f195deaff9a457974f6fc94e
Mohammad Khan <mohammad.khan@gmx.us>
2018-02-12 13:13:46 -0800

    added test cases to cover decimal value for quantity

--- app/models/lineitem.rb
+++ app/models/lineitem.rb
@@ -14,9 +14,17 @@ class Lineitem < ApplicationRecord
 
   before_save :calculate_price_in_cents
 
+  validates :quantity, numericality: { greater_than: 0 }
+
+  validate :decimal_value_of_quantity
 
   def calculate_price_in_cents
     # TODO: put actual logic
     self.price_in_cents = 1256
+  end
+
+  def decimal_value_of_quantity
+    value = self.quantity * (10 ** self.sku.decimal_factor)
+    errors.add(:quantity, "Please check quantity decimal value") unless value.to_i == value
   end
 end
--- spec/models/lineitem_spec.rb
+++ spec/models/lineitem_spec.rb
@@ -17,6 +17,13 @@ RSpec.describe Lineitem, type: :model do
       lineitem.quantity = 1.3
       expect(lineitem.valid?).to eq(false)
     end
+
+    it "should return decimal value if decimal_factor has proper value" do
+      lineitem.sku.decimal_factor = 2
+      lineitem.quantity = 1.32
+      expect(lineitem.valid?).to eq(true)
+    end
+
   end
 
   describe "#price_in_cents"
