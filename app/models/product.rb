f4acd73c0a2d5c67e51fbb2bff201921e556f399
Mohammad Khan <mohammad.khan@gmx.us>
2018-02-13 13:08:17 -0800

    Added test cases to cover product's association with skus and product_categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,6 @@ class Product < ApplicationRecord
   #
   # TODO
   #   - Add validations
-  #   - Add index and unique constraints
-  #   - Association with Lineitem
-  #   - Association with ProductCategory
   #
 
   has_many :skus
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:skus) }
+  it { should have_many(:product_categories) }
+
+  describe "#name" do
+    it "should not be empty"
+  end
 end
