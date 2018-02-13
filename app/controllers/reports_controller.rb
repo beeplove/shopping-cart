6a77c23f48e3e2f572762501f24b65bd0197497e
Mohammad Khan <mohammad.khan@gmx.us>
2018-02-13 09:22:01 -0800

    minor change to make room for generate csv

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -24,14 +24,17 @@ class ReportsController < ApplicationController
   # TODO:
   #   - use params to form actual query
   #   - Take status into account
+  #   - move reusable query to concern
   #
   def product_sold
     start_date = params[:start_date] || '2018-01-01'
     end_date = params[:end_date] || '2018-12-31'
     timespan = (params[:timespan] || 'week').upcase
 
-    jsonator Lineitem.where("created_at >= ? AND created_at <= ?", "2018-02-11 00:00:00", "2018-02-13 23:59:59")
+    report = Lineitem.where("created_at >= ? AND created_at <= ?", "2018-02-11 00:00:00", "2018-02-13 23:59:59")
       .select("sku_id, #{timespan}(created_at) as period, SUM(quantity) as total")
       .group('sku_id, period')
+
+    jsonator report
   end
 end
