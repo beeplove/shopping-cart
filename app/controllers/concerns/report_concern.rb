ed19cdae5304f25f01f57569854896a5415f32a1
Mohammad Khan <mohammad.khan@gmx.us>
2018-02-13 10:49:39 -0800

    some refactoring by moving report controller code to concern

--- app/controllers/concerns/report_concern.rb
+++ app/controllers/concerns/report_concern.rb
@@ -1,14 +1,41 @@
 module ReportConcern
   extend ActiveSupport::Concern
 
+  #
+  # TODO
+  #   - keeping the report very simple for now, consider joining with Sku and Product for more verbose report
+  #
   def get_product_sold_data start_date=nil, end_date=nil, step=nil
-    start_date = (start_date || '2018-01-01').to_datetime
-    end_date = (end_date || '2018-12-31').to_datetime
-    step = (step || 'week').upcase
+    start_date  = (start_date || '2018-01-01').to_datetime
+    end_date    = (end_date || '2018-12-31').to_datetime
+    step        = (step || 'week').upcase
 
     Lineitem.where("created_at >= ? AND created_at <= ?", start_date, end_date)
       .select("sku_id, #{step}(created_at) as period, SUM(quantity) as total")
       .group('sku_id, period')
+  end
+
+  #
+  # TODO: Add proper index to optimize this query
+  #
+  def get_category_report_data
+    Customer.joins(
+      orders: [
+        { lineitems: [
+          { sku: [
+            { product: [
+              { product_categories: :category }
+            ]}
+          ]}
+        ]}
+      ]
+    ).select('
+      customers.id,
+      customers.first_name,
+      categories.id as category_id,
+      categories.name as category_name,
+      sum(lineitems.quantity) as total'
+    ).group('customers.id, customers.first_name, categories.id, categories.name')
   end
 
 end
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,29 +2,12 @@ class ReportsController < ApplicationController
   include ReportConcern
 
   def by_category
-    # TODO: Add proper index to optimize this query
-    jsonator Customer.joins(
-      orders: [
-        { lineitems: [
-          { sku: [
-            { product: [
-              { product_categories: :category }
-            ]}
-          ]}
-        ]}
-      ]
-    ).select('
-      customers.id,
-      customers.first_name,
-      categories.id as category_id,
-      categories.name as category_name,
-      sum(lineitems.quantity) as total'
-    ).group('customers.id, customers.first_name, categories.id, categories.name')
+    jsonator get_category_report_data
   end
 
   #
   # TODO:
-  #   - Take status into account
+  #   - Consider Lineitem#status when generating report
   #   - validate params
   #
   def product_sold
