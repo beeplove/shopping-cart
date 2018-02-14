1047fd795972d14aba4d4c49f7e700c1d896fbfb
Mohammad Khan <mohammad.khan@gmx.us>
2018-02-13 19:55:49 -0800

    Added test cases for ReportsController#product_sold

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe ReportsController, type: :controller do
     end
   end
 
+
+  describe "GET #product_sold" do
+    it "returns success response for happy scenario" do
+      get :product_sold
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns with error response when start_date is invalid" do
+      get :product_sold, params: {start_date: "YYYY-MM-DD"}
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
 end
